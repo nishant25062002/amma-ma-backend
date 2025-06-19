63292272d8ae69c9f6e96e1c69016bbc40e42246
nishant25062002 <mainwalnishant9@gmail.com>
2025-06-19 17:35:41 +0530

    Resolving Error

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 
 // import productRoutes from './routes/productRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import subscriberRoutes from './routes/subscriberRoutes.js';
+import orderRoutes from "./routes/orderRoutes.js";
+import subscriberRoutes from "./routes/subscriberRoutes.js";
 // import contentRoutes from './routes/contentRoutes.js';
 
 dotenv.config();
@@ -18,11 +18,20 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send({ message: "Amma-ma server is running!!" });
 });
+
 // Routes
-// app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/subscribers', subscriberRoutes);
-// app.use('/api/content', contentRoutes);
+// app.use('/products', productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/subscribers", subscriberRoutes);
+// app.use('/content', contentRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Amma-ma backend running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`🚀 Amma-ma backend running on port ${PORT}`)
+);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err); // log error details
+  res.status(500).json({ error: err.message });
+});
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,8 +7,8 @@ import {
 
 const router = express.Router();
 
-router.post("/", createOrder); // POST: Create new order
-router.get("/", getAllOrders); // GET: Get all orders
-router.get("/:orderId", getOrder); // GET: Get all orders
+router.post("/create-order", createOrder); // POST: Create new order
+router.get("/all-orders", getAllOrders); // GET: Get all orders
+router.get("/get-order/:orderId", getOrder); // GET: Get all orders
 
 export default router;
